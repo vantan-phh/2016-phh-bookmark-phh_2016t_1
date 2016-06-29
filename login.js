a97d59f4c5a3c04cc70988407e98e2457c7cf0c2
naarisan <qazaqaza98@gmail.com>
2016-06-29 12:06:01 +0900

    loginとregisterについて、DBと繋げた

---
+++ login.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var mysql = require('mysql');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+ host     : 'localhost',
+ user     : 'root',
+ database : `phh_test_bkm_1`
+});
+
+var app = express();
+
+app.engine('ejs',ejs.renderFile);
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/static', express.static('public'));
+
+app.post('/register', function (req, res) {
+  if (req.body.username && req.body.password) {
+    res.render('./register.confirm.ejs',
+      {
+        username: req.body.username,
+        password: req.body.password,
+        name: req.body.name
+      }
+    );
+    connection.query(
+      "INSERT INTO `users` (`username`, `password`, `name`) VALUES (?, ?, ?)",
+      [req.body.username, req.body.password, req.body.name],
+    function(error, result, fields){
+      console.log(result);
+    });
+  } else {
+    res.redirect('/static/register.html');
+  }
+});
+
+app.post('/login', function (req, res) {
+  if (req.body.username && req.body.password) {
+    var name, id;
+    var result = connection.query(
+      'SELECT `id`, `name` FROM users WHERE `username` = ? AND `password` = ?',
+      [req.body.username, req.body.password],
+      function(error, result, fields){
+        if (result[0]) {
+          id = result[0].id;
+          name = result[0].name;
+          res.render('./login.ejs',
+            {
+              u_id: id,
+              u_name: name
+            }
+          );
+        }
+      }
+    );
+  } else {
+    res.redirect('/static/login.html');
+  }
+});
+
+app.listen(3000)
