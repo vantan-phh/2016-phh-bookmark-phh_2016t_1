193fb01d0e42059b578aae2987c55d4cbd46966f
Sone Yuuki <yuuki_16n1300032@nnn.ed.jp>
2016-07-19 17:11:47 +0900

    update and insert is confirm

--- routes/create.js
+++ routes/create.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var sha256gen = require('../sha256gen');
 var mysql = require('mysql');
 var connection = require('../connection');
+var urlParser = require('../urlParser');
 var client = require('cheerio-httpcli');
 
 function create(url, comment, userId) {
@@ -11,9 +12,10 @@ function create(url, comment, userId) {
   //http通信のためのインスタンス化
   var client = require('cheerio-httpcli');
   //既に同じURLが入っていないか検証する
+  console.log("test");
   new Promise(function (resolve, reject) {
     connection.query(
-      "SELECT `url` FROM `urls`",
+      "SELECT `id`, `url` FROM `urls`",
       //resultに取得した内容、fieldにステータス？が入る
       function(err, result, field){
         if(err){
@@ -21,74 +23,104 @@ function create(url, comment, userId) {
           return "現在URLを追加することができません";
         }
         console.log(result);
-        result = String(result);
-        console.log(result);
-        if(result.match(url)){
+        var dup = false;
+        for (var i = 0; i < result.length; i++) {
+          if (result[i].url.match(url)) {
+            dup = true;
+            urlId = result[i].id;
+          }
+        }
+        if(dup){
           console.log("このURLは既に存在しています");
-          return "このURLは既に存在しています";
         }
         console.log("resolve");
         resolve();
       }
     );
   }).then(function () {
+    console.log(urlId);
+    if (urlId) {
+      console.log("urlId is ok");
+    } else {
+      console.log("urlId is null");
     //////////////////////////////////////////////////////////////////////////////
-    var image, title, description;
+      var image, title, description;
 
-    //タイトルとか取得
-    var urlSync = client.fetchSync(url);
-    urlSync.$("meta[property='og:description']").each(function(){
-      description = urlSync.$(this).attr("content");
-    });
-    if(!description){
-      urlSync.$("meta[name=description]").each(function(){
+      //タイトルとか取得
+      var urlSync = client.fetchSync(url);
+      urlSync.$("meta[property='og:description']").each(function(){
         description = urlSync.$(this).attr("content");
       });
-    }
-    urlSync.$("meta[property='og:title']").each(function(){
-      title = urlSync.$(this).attr("content");
-    });
-    if(!title){
-      urlSync.$("meta[name=title]").each(function(){
+      if(!description){
+        urlSync.$("meta[name=description]").each(function(){
+          description = urlSync.$(this).attr("content");
+        });
+      }
+      urlSync.$("meta[property='og:title']").each(function(){
         title = urlSync.$(this).attr("content");
       });
-    }
-    if(!title){
-      title = urlSync.$("title").text();
-    }
-    urlSync.$("meta[property='og:image']").each( function(){
-      image = urlSync.$(this).attr("content");
-    });
-    if(image){
-      urlSync.$("meta[name=image]").each( function(){
-        image = $(this).attr("content");
+      if(!title){
+        urlSync.$("meta[name=title]").each(function(){
+          title = urlSync.$(this).attr("content");
+        });
+      }
+      if(!title){
+        title = urlSync.$("title").text();
+      }
+      urlSync.$("meta[property='og:image']").each( function(){
+        image = urlSync.$(this).attr("content");
+      });
+      if(!image){
+        urlSync.$("meta[name=image]").each( function(){
+          image = $(this).attr("content");
+        });
+      }
+      if(!image){image = "No image"};
+      if(!title){title = "No title"};
+      if(!description){description = "No description"};
+
+      //URLやそれが持つデータをを追加
+      return new Promise(function (resolve, reject) {
+        connection.query("INSERT INTO `urls` (`url`, `title`, `description`, `image`) VALUES(?, ?, ?, ?)",
+          [url, title, description, image], function (error, result, fields) {
+            if (!result) {
+              console.log(error);
+              return 1;
+            }
+            urlId = result.insertId;
+            console.log(result.insertId);
+            console.log("resolve");
+            console.log(urlId);
+            resolve();
+        })
       });
     }
-    if(!image){image = "No image"};
-    if(!title){title = "No title"};
-    if(!description){description = "No description"};
-
-    //URLやそれが持つデータをを追加
-    return new Promise(function (resolve, reject) {
-      connection.query("INSERT INTO `urls` (`url`, `title`, `description`, `image`) VALUES(?, ?, ?, ?)",
-        [url, title, description, image], function (error, result, fields) {
-          if (!result) {
-            console.log(error);
-            return 1;
-          }
-          urlId = result.insertId;
-          console.log(result.insertId);
-          console.log("resolve");
-          console.log(urlId);
-          resolve();
-      })
-    });
   }).then(function () {
-    console.log("aaa");
-    console.log(userId, urlId, comment, "aaa");
-    connection.query("INSERT INTO `user_url` (`userId`, `urlId`, `comment`) VALUES(?, ?, ?)",
-      [userId, urlId, comment], function (error, result, fields) {
-        console.log(userId, urlId, comment);
+    console.log(userId, urlId, comment);
+
+    connection.query("SELECT `id` FROM `user_url` WHERE `userId` = ? AND `urlID` = ?",
+      [userId, urlId], function (error, result, fields) {
+        console.log(result);
+        var userUrlId = result[0];
+        if (userUrlId) userUrlId = userUrlId.id;
+        console.log(userUrlId);
+        if (userUrlId) {
+          console.log("update");
+          console.log(comment);
+          var query = connection.query("UPDATE `user_url` SET `comment` = ? WHERE id = ?",
+            [comment, userUrlId], function (error, result, fields) {
+              if (error) console.log(error);
+              console.log(result);
+          });
+          console.log(query);
+        } else {
+          console.log("insert");
+          var query = connection.query("INSERT INTO `user_url` (`userId`, `urlId`, `comment`) VALUES(?, ?, ?)",
+            [userId, urlId, comment], function (error, result, fields) {
+              console.log(result);
+          });
+          console.log(query);
+        }
     });
   })
 }
