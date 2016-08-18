f320f38842d0f690263fcc02dc9d2c585a7b316f
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2016-08-19 02:04:27 +0900

    ぼくのかんがえたさいきょうの検索

--- search.js
+++ search.js
@@ -14,67 +14,116 @@ mysql.connect(function(err) {
 
 function kensaku(id, kenmozi) {
   var kenm = kenmozi.split("\s");
+  var kenmo = [];
+  var urlIdire = [];
+  for(var i = 0; i < kenm.length; i++) {
+    kenmo.push(kenm[i].substr(1).substr(0, kenm[i].length - 2));
+  }
   var comment = [];
   var title = [];
   var flag = 0;
-  for(var l = 0; l < kenm.length; l++) {
-    kozinkensaku(kenm[l]);
-    sosikikensaku(kenm[l]);
-  }
-  function kozinkensaku(kenmozi) {
+  kozinkensaku(kenm[0]);
+  sosikikensaku(kenm[0]);
+
+  function kozinkensaku(kenmozii) {
     mysql.query('SELECT `urlId`, `comment` FROM `userComments` WHERE userId = ?',
     [id], function(err, result) {
       if(err || !result){flag++; return;}
-      commentRes = [];
-      var kenmo = kenmozi.substr(1);
-      kenmo = kenmo.substr(0, kenmo.length - 1);
       for(var j = 0; j < result.length; j++) {
-        if(result[j].comment && result[j].comment.match(kenmo))
-        {comment.push(result[j])};
+        for(var i = 0; i < kenmo.length; i++) {
+          if(result[j].comment && result[j].comment.match(kenmo[i])){
+            comment.push(result[j]);
+            break;
+          };
+        }
       }
       var str = 'SELECT `title`, `id` FROM `urls` WHERE'
       var moziire = [];
+      var urlire = [];
       for(var i = 0; i < result.length; i++) {
-        str += " (title LIKE ? AND id = " + result[i].urlId + ") OR"
-        moziire.push(kenmozi)
+        urlire.push(result[i].urlId)
+      }
+      if(urlIdire[0]) {
+        for(var i = 0; i < urlire.length; i++) {
+          for(var j = 0; j < urlIdire.length; j++) {
+            if(urlire[i] === urlIdire[j]) {
+              urlire.splice(i, 1);
+              i--;
+              break;
+            }
+          }
+        }
+      }else {
+        urlIdire = urlire
+      }
+      for(var i = 0; i < urlire.length; i++) {
+        str += " (title LIKE ? AND id = " + urlire[i] + ") OR"
+        moziire.push(kenmozii)
       }
       str = str.substr(0, str.length - 3);
       mysql.query(str, moziire, function(err, res) {
         if(err || !res){flag++; return;}
-        comment.push(commentRes);
-        title.push(res);
-        kenm.length - 1 <= flag ? kenkensaku() : flag++;
+        for(var i = 0; i < res.length; i++) {
+          title.push(res[i]);
+        }
+        kenm.length <= flag ? kenkensaku() : flag++;
         /*commentRes[num].url_id で urlのid
         commentRes[num].comment で コメント
         res[num].title でタイトル*/
       })
     })
   }
-  function sosikikensaku(kenmozi) {
+
+  function sosikikensaku(kenmozii) {
     mysql.query('SELECT `orgId` FROM `joiningOrgs` WHERE userId = ?', [id],
     function(err, result) {
       if(err || !result){flag++; return;}
       var strC = 'SELECT `comment`, `urlId` FROM `orgComments` WHERE'
       var moziire = [];
+
       for(i = 0; i < result.length; i++) {
-        strC += (" comment LIKE ?  AND orgId = " + result[i].org_id + " OR");
-        moziire.push(kenmozi);
+        strC += " (comment LIKE ? AND orgId = " + result[i].orgId + ") OR";
+        moziire.push(kenmozii);
       }
       strC = strC.substr(0, strC.length - 3);
       mysql.query(strC, moziire, function(err, res){
-        if(err || res){flag++; return;}
+        if(err || !res){flag++; return;}
         var str = 'SELECT `title`, `id` FROM `urls` WHERE'
+        hozon = [];
         moziire = [];
         for(var i = 0; i < res.length; i++) {
-          str += (" (id = " + res[i].urlId + "AND title LIKE ?) OR");
-          moziire.push(kenmozi);
+          hozon.push(res[i].urlId);
+        }
+        hozon = hozon.filter(function (x, i, self) {
+          return self.indexOf(x) === i;
+        });
+        if(urlIdire[0]) {
+          for(var i = 0; i < hozon.length; i++) {
+            for(var j = 0; j < urlIdire.length; j++) {
+              if(hozon[i] === urlIdire[j]) {
+                hozon.splice(i, 1);
+                i--;
+                break;
+              }
+            }
+          }
+        }else {
+          urlIdire = hozon
+        }
+        for(var m = 0; m < hozon.length; m++) {
+          str += " (title LIKE ? AND id = " + hozon[m] + ") OR";
+          moziire.push(kenmozii);
         }
         str = str.substr(0, str.length - 3);
         mysql.query(str, moziire, function(err, re) {
-          if(err || re){flag++; return;}
-          comment.push(res)
-          title.push(re)
-          kenm.length - 1 <= flag ? kenkensaku() : flag++;
+          if(err || !re){flag++; return;}
+          for(var i = 0; i < res.length; i++) {
+            comment.push(res[i])
+          }
+          for(var j = 0; j < re.length; j++) {
+            title.push(re[j])
+          }
+          kenm.length <= flag ? kenkensaku() : flag++;
           /* res[num].comment でコメント, res[num].urlId でURLID
           re[num].title でタイトル, re[num].id でURLID */
         })
@@ -82,9 +131,19 @@ function kensaku(id, kenmozi) {
     });
   }
   function kenkensaku() {
-    console.log(comment);
-    console.log(title);
+    var result = [];
+    //console.log(comment);
+    //console.log(title);
+    for(var i = 0; i < kenmo.length; i ++) {
+      for(var j = 0; j < comment.length; j++) {
+        if(comment[j].comment && comment[j].comment.match(kenmo[i])){result.push(comment[j])};
+      }
+      for(var t = 0; t < title.length; t++) {
+        if(title[t].title && title[t].title.match(kenmo[i])){result.push(title[t])};
+      }
+    }
+    console.log(result);
   }
 }
 
-kensaku(1, "%あるぇ%")
+kensaku(1, "%う%")
