afb85064a0a675fd7ef9ca9fa8784bda9bafc1cc
Sone Yuuki <yuuki_16n1300032@nnn.ed.jp>
2016-08-19 14:53:28 +0900

    receive query

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function (req, res) {
+  query = req.query.q;
   res.render('./search.ejs',{
+    query: query
   });
 });
 
