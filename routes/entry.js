a0890e41f3a2a6b35517a594fd56289114df22c5
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2016-09-06 14:51:40 +0900

    aaa

--- routes/entry.js
+++ routes/entry.js
@@ -3,30 +3,32 @@ var express = require('express');
 var connection = require('../connection');
 var router = express.Router();
 
-router.get('/:id(\\d+)', function (req, res) {
+router.get('/:urlId(\\d+)/:orgId(\\d)', function (req, res) {
   var unity = []; // id, icon, comment で構成されるオブジェクトが入る配列
-  var id = req.params.id;
-  var orgId = //ください
+  var id = req.params.urlId;
+  var orgId = req.params.orgId;
   connection.query("SELECT * FROM `urls` WHERE `id` = ?", [id], function(err, result) {
-    if(err)return "指定のエントリーが存在しない可能性があります";
-    connection.query("SELECT `userId` `comment` FROM `orgsComment` WHERE urlId = ? AND orgId = ?", [result.id, orgId]
+    if(err)console.log(err);
+    connection.query("SELECT `userId` `comment` FROM `orgsComment` WHERE urlId = ? AND orgId = ?", [result.id, orgId],
     function(err, res) {
-      if(err)return "うえい";
-      var str = "SELECT `icon` FROM `users` WHERE id = ?"
+      if(err)console.log(err);
+      var str = "SELECT `icon` FROM `users` WHERE id = "
       for(var i = 0; i < res.length; i++) {
         str += res[i].userId + " OR id = "
       }
       connection.query(str, function(err, re) {
-        if(err)return "えええ？"
+        if(err)console.log(err);
         for(var i = 0; i < re.length; i++) {
           unity[i] = {id: res[i].userId, icon: re[i].icon, comment: res[i].comment};
         }
-        result // urlの内容
-        unity // 結果
+        console.log(unity[0]);
+        res.render('./entry.ejs', {
+          union: unity,
+          urlResult: result
+        });
       })
     })
   });
-  res.render('./entry.ejs');
 });
 
 module.exports = router;
