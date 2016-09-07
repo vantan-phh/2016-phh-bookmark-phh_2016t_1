357bd0733475e6b46c465b29d0194eb1294563f8
Sone Yuuki <yuuki_16n1300032@nnn.ed.jp>
2016-09-07 16:32:01 +0900

    rename

--- routes/joiningOrgs.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/:id(\\d+)', function (req, res) {
-  var userId = req.params.id;
-  res.render('./joiningOrgs.ejs');
-});
-
-module.exports = router;
