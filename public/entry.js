771c479008e7124153c4cf5c7770feb5625a00e4
ohtasoji0715 <souji_16n1300015@nnn.ed.jp>
2016-09-08 03:20:48 +0900

    create entry.ejs

--- public/entry.js
+++ public/entry.js
@@ -1,3 +1,30 @@
 $(function(){
+  var url = $(':hidden[name="url"]').val();
+  var orgId =$(':hidden[name="orgId"]').val();
 
+  $('#addbtn').on('click',function(){
+    var inputComment = $("#inputComment").val();
+    console.log(inputComment);
+    console.log(orgId);
+    $.ajax({
+      type: "POST",
+      url: "/create/org",
+      dataType: "text",
+      data: {
+        "url": url,
+        "comment": inputComment,
+        "orgId" : orgId
+      },
+      success: function(data, textStatus){
+        //$('#pii').text(data);
+        console.log(data);
+        console.log(textStatus);
+        console.log("成功!!!");
+      },
+      error: function(xhr, textStatus, errorThrown){
+        // エラー処理
+      }
+    });
+
+  });
 });
