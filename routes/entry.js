f80c08fc600e0951f8f16ee03e17edd10cf40a38
ohtasoji0715 <souji_16n1300015@nnn.ed.jp>
2016-09-07 17:18:45 +0900

    change entry.js

--- routes/entry.js
+++ routes/entry.js
@@ -4,36 +4,35 @@ var connection = require('../connection');
 var router = express.Router();
 
 router.get('/:urlId(\\d+)/:orgId(\\d)', function (req, res) {
-  var unity = []; // id, icon, comment で構成されるオブジェクトが入る配列
-  var id = req.params.urlId;
-  var orgId = req.params.orgId;
-  connection.query("SELECT * FROM `urls` WHERE `id` = ?", [id], function(err, result) {
-    if(err)console.log("s");
-    connection.query("SELECT `userId` `comment` FROM `orgComments` WHERE `urlId` = ? AND `orgId` = ?", [result[0].id, orgId],
-    function(err, res) {
-      if(err)console.log("a");
-      var str = "SELECT `icon` FROM `users`"
-      console.log(res.length);
-      for(var i = 0; i < res.length; i++) {
-        if(i == 0) str += " WHERE id = "
-        str += res[i].userId
-        if(!(i == res.length - 1)) {
-          str += " OR id = "
-        }
-      }
-      connection.query(str, function(err, re) {
-        if(err)console.log(err);
-        for(var i = 0; i < re.length; i++) {
-          unity[i] = {id: res[i].userId, icon: re[i].icon, comment: res[i].comment};
-        }
-        console.log(unity[0]);
-        res.render('./entry.ejs', {
-          union: unity,
-          urlResult: result
-        });
-      })
-    })
-  });
+ var unity = []; // id, icon, comment で構成されるオブジェクトが入る配列
+ var id = req.params.urlId;
+ var orgId = req.params.orgId;
+ connection.query("SELECT * FROM `urls` WHERE `id` = " + id, function(err, result) {
+   if(err)console.log(err);
+   connection.query("SELECT `userId`, `comment` FROM `orgComments` WHERE `urlId` = ? AND `orgId` = ?", [result[0].id, orgId],
+   function(err, resu) {
+     if(err || resu[0] == undefined)console.log("a");
+     var str = "SELECT `icon` FROM `users`"
+     for(var i = 0; i < resu.length; i++) {
+       if(i == 0) str += " WHERE id = "
+       str += resu[i].userId
+       if(!(i == resu.length - 1)) {
+         str += " OR id = "
+       }
+     }
+     connection.query(str, function(err, re) {
+       if(err)console.log(err);
+       for(var i = 0; i < re.length; i++) {
+         unity[i] = {id: resu[i].userId, icon: re[i].icon, comment: resu[i].comment};
+       }
+       console.log(result[0]);
+       res.render('./entry.ejs', {
+         union: unity,
+         urlResult: result[0]
+       });
+     })
+   })
+ });
 });
 
 module.exports = router;
