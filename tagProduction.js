7be53e38f16e037ae16e8c1136673bd74337a40c
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2016-09-13 00:49:53 +0900

    地獄の業火より生還せし僕

---
+++ tagProduction.js
@@ -0,0 +1,46 @@
+var connection = require('./connection');
+
+function tagProduction(tagName, urlId, orgId) {
+  return new Promise( (resolve, reject) => {
+    connection.query("SELECT `id` FROM `tag` WHERE tagName = '" + tagName + "'",
+    function(err, result) {
+      if(err)return "タグ生成失敗";
+      if(result[0] == undefined) {
+        connection.query("INSERT INTO tag(tagName) values('" + tagName + "')",
+        function(err, res) {
+          if(err)return "タグ生成失敗"
+          connection.query("SELECT `id` FROM tag WHERE tagName = '" + tagName + "'",
+          function(err, re) {
+            if(err)return "タグ生成失敗"
+            connection.query("INSERT INTO urlTag(urlId, tagId, orgId) values(" + urlId + "," + re[0].id + "," + orgId + ")",
+            function(err, res) {
+              if(err)return "タグ生成失敗"
+              connection.query("SELECT `id` FROM urlTag WHERE urlId = " + urlId + " AND tagId = " + re[0].id + " AND orgId = " + orgId,
+              function(err, res) {
+                resolve(res[0].id);
+              });
+            });
+          });
+        });
+      }else {
+        connection.query("SELECT `id` FROM `urlTag` WHERE urlId = " + urlId + " AND tagId = " + result[0].id + " AND orgId = " + orgId,
+        function(err, res) {
+          if(err)return "タグ生成失敗"
+          if(res[0] == undefined) {
+            connection.query("INSERT INTO urlTag(urlId, tagId, orgId) values(" + urlId + "," + result[0].id + "," + orgId + ")",
+            function(err, res) {
+              if(err)return "タグ生成失敗"
+              connection.query("SELECT `id` FROM urlTag WHERE urlId = " + urlId + " AND tagId = " + result[0].id + " AND orgId = " + orgId,
+              function(err, res) {
+                if(err)return "タグ生成失敗";
+                resolve(res[0].id);
+              });
+            });
+          }else {
+            resolve(res[0].id);
+          }
+        });
+      }
+    });
+  });
+};
