0b43fdd27a3db815738bbfbdd0cc132ad55f7b66
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2016-09-14 11:38:20 +0900

    タグ検索

--- tagSearch.js
+++ tagSearch.js
@@ -1,24 +1,55 @@
 var connection = require('./connection');
 
 function tagSearch(userId, tagName) {
-  return new Promise( (resolve, reject) => {
-    connection.query("SELECT `orgId` FROM `joiningOrgs` WHERE userId = " + userId,
-    function(err, result) {
-      if(err || result[0] == undefined)return "検索失敗"
-      connection.query("SELECT `id` FROM `tag` WHERE `tagName` = '" + tagName + "'",
-      function(err, res) {
-        if(err || res[0] == undefined)return "検索失敗"
-        str = "SELECT `urlId` FROM `urlTag` WHERE `tagId` = " + res[0].id + " AND (";
-        for(var i = 0; i < result.length; i++) {
-          str += "`orgId` = " + result[i].orgId + " OR ";
+  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT `orgId` FROM `joiningOrgs` WHERE userId = ?', [userId],
+      function(err, sqlResult){
+        try{
+          var result = {};
+          if(err) throw err;
+          if(sqlResult[0] === undefined) throw new Error('select empty');
+          result.first = sqlResult;
+          resolve(result);
+        }catch(err) {
+          console.dir(err);
+          reject(err);
         }
-        str = str.substr(0, str.length - 4);
-        str += ")";
-        connection.query(str, function(err, res) {
-          if(err || res[0] == undefined) return "検索失敗";
-          resolve(res);
-        });
       });
-    });
-  });
+    }).then(result => new Promise((resolve, reject) => {
+      connection.query('SELECT `id` FROM `tag` WHERE `tagName`=?', [tagName],
+      function(err, sqlResult){
+        try {
+          if(err) throw err;
+          if(sqlResult[0] === undefined) throw new Error('tag empty')
+          result.second = sqlResult;
+          resolve(result);
+        }catch(e) {
+          console.dir(e);
+          reject(e);
+        }
+      });
+    })).then(result => new Promise((resolve, reject) => {
+      var ary = [result.second[0].id]
+      var str = "SELECT `urlId` FROM `urlTag` WHERE `tagId` = ? AND (";
+      for(var i = 0; i < result.first.length; i++) {
+        str += "`orgId` = ? OR ";
+        ary[i + 1] = result.first[i].orgId;
+      }
+      str = str.substr(0, str.length - 4);
+      str += ")";
+      connection.query(str, ary, function(err, res) {
+        try {
+          if(err) throw err;
+          if(res[0] === undefined) throw new Error('ふげぇ')
+          resolve(res);
+        }catch(e) {
+          console.dir(e);
+          reject(e);
+        }
+      })
+    })).then(result => {
+      resolve(result);
+    })
+  })
 }
