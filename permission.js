f77466e2643b31cf666dcfd0cf4ac7c59fd848d0
ohtasoji0715 <souji_16n1300015@nnn.ed.jp>
2016-09-14 17:01:46 +0900

    delete permission.js

--- permission.js
+++
@@ -1,5 +0,0 @@
-var connection = require('./connection');
-//orgIdに存在するuserIdに権限を与える関数
-function distribution (orgId, userId) {
-  connection.query("UPDATE joiningOrgs SET permission = 2 WHERE orgId = ? AND userId = ?",[orgId, userId], function(err) {if(err)console.log(err)})
-}
