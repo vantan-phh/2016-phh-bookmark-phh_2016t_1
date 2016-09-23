d73e741a8c69eaf9ae44f502a37d6f70098ae29c
Sone Yuuki <yuuki_16n1300032@nnn.ed.jp>
2016-09-23 17:52:16 +0900

    重複した際にエラーを吐くようにした

--- routes/setting.js
+++ routes/setting.js
@@ -19,6 +19,50 @@ function queryPromise(sqlstr, array) {
   });
 }
 
+function nameQuery(name, userId) {
+  return new Promise( (resolve, reject) => {
+    queryPromise("SELECT COUNT(name) AS `count` FROM users WHERE `name` = ?", [name])
+    .then((result) => {
+      if (result[0].count == 0) {
+        connection.query( "UPDATE `users` SET `name` = ?, `time_updated` = ? WHERE id = ?", [name, +new Date(), userId],
+          function (error, result, fields) {
+            console.log(result);
+            if (result) resolve(result);
+            if (error) {
+              console.log(error);
+              reject(error)
+            };
+          }
+        );
+      } else {
+        reject("name duplicate");
+      }
+    })
+  });
+}
+
+function emailQuery(email, userId) {
+  return new Promise( (resolve, reject) => {
+    queryPromise("SELECT COUNT(email) AS `count` FROM users WHERE `email` = ?", [email])
+    .then((result) => {
+      if (result[0].count == 0) {
+        connection.query( "UPDATE `users` SET `name` = ?, `time_updated` = ? WHERE id = ?", [email, +new Date(), userId],
+          function (error, result, fields) {
+            console.log(result);
+            if (result) resolve(result);
+            if (error) {
+              console.log(error);
+              reject(error)
+            };
+          }
+        );
+      } else {
+        reject("email duplicate");
+      }
+    })
+  });
+}
+
 router.get('/', function (req, res) {
   var warn = "";
   res.render('./setting.ejs');
@@ -41,10 +85,17 @@ router.post('/', function (req, res) {
   ).then((result) => {
     if (password === result[0].password) {
       if (displayName) promiseArr.push(["UPDATE `users` SET `displayName` = ?, `time_updated` = ? WHERE id = ?", [displayName, +new Date(), userId]]);
-      if (name) promiseArr.push(["UPDATE `users` SET `name` = ?, `time_updated` = ? WHERE id = ?", [name, +new Date(), userId]]);
-      if (email) promiseArr.push(["UPDATE `users` SET `email` = ?, `time_updated` = ? WHERE id = ?", [email, +new Date(), userId]]);
+      if (name) promiseArr.push(["name"]);
+      if (email) promiseArr.push(["email"]);
       if (newpassword) promiseArr.push(["UPDATE `users` SET `password` = ?, `time_updated` = ? WHERE id = ?", [newpassword, +new Date(), userId]]);
-      Promise.all(promiseArr.map((arr) => {return queryPromise(arr[0], arr[1])})).then(() => {
+      Promise.all(promiseArr.map((arr) => {
+        if (arr[0] == "name") {
+          return nameQuery(name, userId);
+        } else if (arr[0] == "email") {
+          return emailQuery(email, userId);
+        }
+        return queryPromise(arr[0], arr[1])
+      })).then(() => {
         console.log("success");
         res.status(200).send("success");
       }).catch((error) => {
